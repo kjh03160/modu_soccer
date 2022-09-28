a65d89fb13360f3e2a03550b743969ab80d7fc29
juna.k <juna.k@navercorp.com>
2022-09-28 22:24:52 +0900

    [fix] fix jar file name & deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
-APP_NAME=modu-soccer
+APP_NAME=soccer
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
