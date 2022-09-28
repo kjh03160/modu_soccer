fc0c51ccf9304d818ea519b13b32e955437d5660
juna.k <juna.k@navercorp.com>
2022-09-29 00:52:39 +0900

    [feat] add common api resonse and error response

---
+++ src/main/java/com/modu/soccer/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,56 @@
+package com.modu.soccer.advice;
+
+
+import static com.modu.soccer.exception.ErrorCode.DUPLICATE_RESOURCE;
+import static com.modu.soccer.exception.ErrorCode.INVALID_PARAM;
+import static com.modu.soccer.exception.ErrorCode.UNKNOWN_ERROR;
+
+import com.modu.soccer.exception.CustomException;
+import com.modu.soccer.exception.ErrorResponse;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+	@ExceptionHandler(value = {ConstraintViolationException.class,
+		DataIntegrityViolationException.class})
+	protected ResponseEntity<ErrorResponse> handleDataException() {
+		log.error("handleDataException throw Exception : {}", DUPLICATE_RESOURCE);
+		return ErrorResponse.toResponseEntity(DUPLICATE_RESOURCE);
+	}
+
+	@ExceptionHandler(value = {CustomException.class})
+	protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+		log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
+		return ErrorResponse.toResponseEntity(e.getErrorCode());
+	}
+
+
+	@ExceptionHandler(value = {MethodArgumentNotValidException.class})
+	protected ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(
+		MethodArgumentNotValidException e) {
+		String validationMsg = e.getFieldErrors().stream()
+			.map(fe -> fe.getField() + " " + fe.getRejectedValue() + " " + fe.getDefaultMessage())
+			.collect(Collectors.joining(", "));
+		ErrorResponse errorResponse = ErrorResponse.builder()
+			.code(INVALID_PARAM.getCode())
+			.message(validationMsg)
+			.build();
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(value = {Exception.class})
+	protected ResponseEntity<ErrorResponse> handleUnKnownException(Exception e) {
+		log.error("Unknown Exception : {}", e.getMessage());
+		return ErrorResponse.toResponseEntity(UNKNOWN_ERROR);
+	}
+}
