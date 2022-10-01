ff64743c27de25b250efdaccf921d793887362ff
kjh03160 <kis03160@gmail.com>
2022-10-01 21:06:23 +0900

    [fix] json key

--- src/main/java/com/modu/soccer/domain/UserDto.java
+++ src/main/java/com/modu/soccer/domain/UserDto.java
@@ -1,5 +1,6 @@
 package com.modu.soccer.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.modu.soccer.entity.User;
 import com.modu.soccer.enums.AuthProvider;
 import lombok.Builder;
@@ -9,10 +10,13 @@ import lombok.Getter;
 @Builder
 public class UserDto {
 	private String email;
+	@JsonProperty("profile_url")
 	private String profileURL;
 	private String name;
+	@JsonProperty("is_pro")
 	private boolean isPro;
 	private Integer age;
+	@JsonProperty("auth_provider")
 	private AuthProvider authProvider;
 
 	public static UserDto fromEntity(User user) {
