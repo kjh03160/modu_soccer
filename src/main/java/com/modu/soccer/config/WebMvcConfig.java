194e56ab0c1686df0a760d1f169b178adb3962cb
kjh03160 <kis03160@gmail.com>
2022-10-02 21:54:00 +0900

    [feat] add jwt auth filter

---
+++ src/main/java/com/modu/soccer/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.modu.soccer.config;
+
+import com.modu.soccer.jwt.JwtAuthenticateFilter;
+import com.modu.soccer.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	private final JwtProvider jwtProvider;
+
+	@Bean
+	public FilterRegistrationBean filterRegistration() {
+		FilterRegistrationBean<JwtAuthenticateFilter> authenticateFilter = new FilterRegistrationBean<>(
+			new JwtAuthenticateFilter(jwtProvider));
+		authenticateFilter.setOrder(1);
+		authenticateFilter.addUrlPatterns("/api/v1/*");
+		return authenticateFilter;
+	}
+}
---
+++ src/main/java/com/modu/soccer/jwt/JwtAuthenticateFilter.java
@@ -0,0 +1,62 @@
+package com.modu.soccer.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.modu.soccer.domain.ApiResponse;
+import com.modu.soccer.exception.ErrorCode;
+import com.modu.soccer.exception.ErrorResponse;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@AllArgsConstructor
+public class JwtAuthenticateFilter extends OncePerRequestFilter {
+
+	private JwtProvider jwtProvider;
+	private final ObjectMapper objectMapper = new ObjectMapper();
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws IOException, ServletException {
+		String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+		try {
+			if (jwtProvider.isTokenExpired(jwtProvider.getJwtTokenFromHeader(authorizationHeader))){
+				ApiResponse<?> apiResponse = setResponse(response, ErrorCode.ACCESS_TOKEN_EXPIRED);
+				objectMapper.writeValue(response.getWriter(), apiResponse);
+				return;
+			}
+		}  catch (Exception e) {
+			ApiResponse<?> apiResponse = setResponse(response, ErrorCode.UNKNOWN_ERROR);
+			log.error("jwt token validate error: {}", e.getMessage());
+			objectMapper.writeValue(response.getWriter(), apiResponse);
+			return;
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request)
+		throws ServletException {
+		String path = request.getRequestURI();
+		return path.startsWith("/api/v1/oauth") || path.equals("/api/v1/user/token");
+	}
+
+	private ApiResponse<?> setResponse(HttpServletResponse response, ErrorCode errorCode) {
+		response.setStatus(errorCode.getHttpStatus().value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding("UTF-8");
+		return ApiResponse.errorResponse(
+			ErrorResponse.builder()
+				.code(errorCode.getCode())
+				.message(errorCode.getMsg())
+				.build()
+		);
+	}
+}
