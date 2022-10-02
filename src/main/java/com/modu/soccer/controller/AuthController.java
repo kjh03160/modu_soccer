f12fb682315663faad78159601f22ffb46283075
kjh03160 <kis03160@gmail.com>
2022-10-02 21:50:58 +0900

    [feat] add jwt config

--- src/main/java/com/modu/soccer/controller/AuthController.java
+++ src/main/java/com/modu/soccer/controller/AuthController.java
@@ -5,10 +5,12 @@ import com.modu.soccer.domain.response.KakaoUserInfoResponse;
 import com.modu.soccer.domain.request.OauthLoginRequest;
 import com.modu.soccer.domain.UserDto;
 import com.modu.soccer.entity.User;
+import com.modu.soccer.jwt.JwtUtil;
 import com.modu.soccer.service.KakaoOauthService;
 import com.modu.soccer.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,13 +24,14 @@ public class OauthController {
 
 	private final KakaoOauthService kakaoOauthService;
 	private final UserService userService;
+	private final JwtUtil jwtUtil;
 
 	@GetMapping("/oauth/callback/kakao")
 	public ApiResponse<?> kakaoCallback(@RequestParam String code) {
 		String token = kakaoOauthService.requestOauthToken(code);
 		KakaoUserInfoResponse userInfo = kakaoOauthService.getUserInfo(token);
 		User user = userService.oauthLogin(OauthLoginRequest.from(userInfo));
-		return ApiResponse.withBody(UserDto.fromEntity(user));
+		return ApiResponse.withBody(UserDto.of(user, jwtUtil.createAccessToken(user)));
 	}
 }
 
