2871318f79d2b8949fc128aa96a0752d8cb2865c
kjh03160 <kis03160@gmail.com>
2022-10-15 23:43:59 +0900

    [fix] convert formation to name in json

--- src/main/java/com/modu/soccer/enums/FormationName.java
+++ src/main/java/com/modu/soccer/enums/FormationName.java
@@ -1,5 +1,11 @@
 package com.modu.soccer.enums;
 
+import static java.util.stream.Collectors.toMap;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.Map;
+import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,5 +18,21 @@ public enum FormationName {
 
 	;
 
+	public static final Map<String, FormationName> stringToEnum =
+		Stream.of(values()).collect(toMap(FormationName::getFormation, formationName -> formationName));
+
 	private final String formation;
+
+	@JsonCreator
+	public static FormationName fromString(String symbol) {
+		if (FormationName.stringToEnum.containsKey(symbol)) {
+			return FormationName.stringToEnum.get(symbol);
+		}
+		throw new IllegalArgumentException(String.format("%s is invalid formation", symbol));
+	}
+
+	@JsonValue
+	public String getFormation() {
+		return formation;
+	}
 }
