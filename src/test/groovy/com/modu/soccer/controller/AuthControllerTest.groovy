8ea6e2b0f6f513514b5b8837cc8f1692175a552d
kjh03160 <kis03160@gmail.com>
2022-11-05 22:03:06 +0900

    [fix] fix auth controller test code

--- src/test/groovy/com/modu/soccer/controller/AuthControllerTest.groovy
+++ src/test/groovy/com/modu/soccer/controller/AuthControllerTest.groovy
@@ -2,20 +2,18 @@ package com.modu.soccer.controller
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.modu.soccer.TestUtil
 import com.modu.soccer.domain.ApiResponse
 import com.modu.soccer.domain.request.OauthLoginRequest
 import com.modu.soccer.domain.request.TokenRefreshRequest
 import com.modu.soccer.domain.response.AuthenticateResponse
 import com.modu.soccer.domain.response.KakaoUserInfoResponse
-import com.modu.soccer.entity.User
-import com.modu.soccer.enums.MDCKey
 import com.modu.soccer.enums.TokenType
-import com.modu.soccer.jwt.JwtInterceptor
 import com.modu.soccer.jwt.JwtProvider
 import com.modu.soccer.repository.UserRepository
 import com.modu.soccer.service.AuthService
 import com.modu.soccer.service.KakaoOauthService
-import org.slf4j.MDC
+import com.modu.soccer.utils.UserContextUtil
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -29,7 +27,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import spock.lang.Specification
 
 @AutoConfigureMockMvc
-@WebMvcTest(controllers = [AuthController])
+@WebMvcTest(controllers = [AuthController, JwtProvider])
 @TestPropertySource(properties = [
         "jwt.secret_key=JvzErMQQTbPz3KrN/Lx3Yl6zq1WgySlrD+UbWB0ALXIuP5gsTjz98bB/yvpCRpj0c5Hv4Vsus03mrzMdPgJAVA==",
         "jwt.expire_in.access_token=600000",
@@ -38,6 +36,7 @@ import spock.lang.Specification
 class AuthControllerTest extends Specification {
     private final String KAKAO_CALLBACK = "/api/v1/oauth/callback/kakao"
     private final String REFRESH_ACCESS_TOKEN = "/api/v1/user/token"
+    private final String LOGOUT = "/api/v1/user/logout"
 
     @Autowired
     protected MockMvc mvc
@@ -46,21 +45,18 @@ class AuthControllerTest extends Specification {
     @SpringBean
     private AuthService authService= Stub();
     @SpringBean
-    private JwtProvider jwtProvider= Stub();
-    @SpringBean
-    private JwtInterceptor jwtInterceptor= Stub();
-    @SpringBean
     private UserRepository userRepository= Stub();
+    @Autowired
+    private JwtProvider jwtProvider;
     private ObjectMapper objectMapper = new ObjectMapper();
 
     def "kakaoCallback"() {
         given:
-        def u = getBasicUser()
+        def u = TestUtil.getUser(1l, "email")
 
         kakaoOauthService.requestOauthToken("code") >> "kakao_token"
         kakaoOauthService.getUserInfo("kakao_token") >> getKakaoResponse()
         authService.oauthLogin(_ as OauthLoginRequest) >> u
-        jwtProvider.createTokenOfType(u, TokenType.AUTH_ACCESS_TOKEN) >> "access_token"
 
         when:
         def result = mvc.perform(MockMvcRequestBuilders.get(KAKAO_CALLBACK)
@@ -74,18 +70,19 @@ class AuthControllerTest extends Specification {
         then:
         noExceptionThrown()
         response.getCode() == 0
-        response.getContents().getAccessToken() == "access_token"
+        response.getContents().getAccessToken() != null
         response.getContents().getEmail() == u.getEmail()
     }
 
     def "refreshAccessToken"() {
         given:
         def request = new TokenRefreshRequest("token")
-        def header = "Bearer token"
-        def u = getBasicUser()
-        jwtProvider.isTokenExpired(_ as String) >> {MDC.put(MDCKey.USER_ID.getKey(), u.getId().toString()); return true}
+        def u = TestUtil.getUser(1l, "email")
+        def expiredToken = "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoiMSIsImVtYWlsIjoia2lzMDMxNjBAZGF1bS5uZXQiLCJpc3MiOiJtb2R1X3NvY2NlciIsImV4cCI6MTY2NDcwNDkwMiwiaWF0IjoxNjY0NzA0MzAyfQ.rebzk8U0QIDM2un6GNW5CKGEr70-iHtJDS0hFdautZwCwpE7FibANMErVdwJy_DUwVe-i5kKBcryQIHqkrt1dw"
+        def header = "Bearer " + expiredToken
+
+        u.setRefreshToken(request.getRefreshToken())
         authService.refreshUserToken(_, request.getRefreshToken()) >> u
-        jwtProvider.createTokenOfType(u, TokenType.AUTH_ACCESS_TOKEN) >> "access_token"
 
         when:
         def result = mvc.perform(MockMvcRequestBuilders.post(REFRESH_ACCESS_TOKEN)
@@ -100,15 +97,15 @@ class AuthControllerTest extends Specification {
         then:
         noExceptionThrown()
         response.getCode() == 0
-        response.getContents().getAccessToken() == "access_token"
+        response.getContents().getAccessToken() != null
         response.getContents().getEmail() == u.getEmail()
     }
 
     def "refreshAccessToken - access token not expired"() {
         given:
+        def u = TestUtil.getUser(1l, "email")
         def request = new TokenRefreshRequest("token")
-        def header = "Bearer token"
-        jwtProvider.isTokenExpired(_ as String) >> {MDC.put(MDCKey.USER_ID.getKey(), _ as String); return false}
+        def header = "Bearer " + jwtProvider.createTokenOfType(u, TokenType.AUTH_ACCESS_TOKEN)
 
         when:
         def result = mvc.perform(MockMvcRequestBuilders.post(REFRESH_ACCESS_TOKEN)
@@ -125,11 +122,29 @@ class AuthControllerTest extends Specification {
         response.getCode() == 40000
     }
 
-    def getBasicUser() {
-        def u = new User()
-        u.setId(1l)
-        u.setEmail("foo@example.com")
-        return u
+    def "logout"() {
+        given:
+        def u = TestUtil.getUser(1l, "email")
+        def token = jwtProvider.createTokenOfType(u, TokenType.AUTH_ACCESS_TOKEN)
+        def header = "Bearer " + token
+
+        UserContextUtil.setUser(u)
+        userRepository.findById(u.getId()) >> Optional.of(u)
+
+        authService.logoutCurrentUser() >> null
+
+        when:
+        def result = mvc.perform(MockMvcRequestBuilders.get(LOGOUT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, header))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn()
+                .getResponse()
+
+        def response = objectMapper.readValue(result.getContentAsString(), new TypeReference<ApiResponse<?>>(){})
+        then:
+        noExceptionThrown()
+        response.getCode() == 0
     }
 
     def getKakaoResponse() {
